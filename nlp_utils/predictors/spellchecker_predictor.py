ba14db45af99fe1871b72fa21c44fc42f82483dd
Никита <nsykhr@gmail.com>
2020-07-09 12:21:07 +0300

    Polished the predictor.

--- nlp_utils/predictors/spellchecker_predictor.py
+++ nlp_utils/predictors/spellchecker_predictor.py
@@ -1,10 +1,11 @@
+import os
 import re
 import numpy as np
 from itertools import product
 from string import punctuation
 from overrides import overrides
 from nltk import edit_distance, sent_tokenize
-from typing import List, Tuple, Set, Iterator, Union, Callable, Optional
+from typing import List, Tuple, Set, Iterator, Sequence, Union, Optional
 
 from allennlp.models import Model
 from allennlp.predictors import Predictor
@@ -12,19 +13,34 @@ from allennlp.common.util import JsonDict
 from allennlp.data import DatasetReader, Instance
 
 punctuation += '«»—…“”№–'
-punctuation_to_keep = re.compile(r'[.,]*([!"#$%&\'()*+\-/:;<=>?@\[\]\\^_`{}|~«»—…“”№–]*)')
+punctuation_to_keep = re.compile(r'[.,]*([§!"#$%&\'()*+\-/:;<=>?@\[\]\\^_`{}|~«»—…“”№–]*)')
 
-NAMED_ENTITIES = {
-    'PER',
-    'PERSON',
-    'NORP',
-    'FAC',
-    'ORG',
-    'LOC',
-    'PRODUCT',
-    'EVENT',
-    'WORK_OF_ART',
-    'LAW'
+BEFORE_PUNCT = set('(<[{«“№')
+AFTER_PUNCT = set('.,!%):;>?]}»…”')
+
+SINGLE_CHAR_WORD = {
+    'н': 'и',
+    'й': 'и',
+    'з': '3',
+    'З': '3',
+    '(': 'С',
+    '0': 'о',
+    '&': 'в',
+    'р': 'в',
+    '<': '«',
+    '>': '»',
+    '|': '1',
+    'щ': 'и',
+    'е': 'с',
+    'ё': 'с'
+}
+
+DOUBLE_CHAR_WORD = {
+    'ша': 'на',
+    'па': 'на',
+    'ее': 'её',
+    '3а': 'за',
+    '$а': 'за'
 }
 
 
@@ -32,13 +48,11 @@ class SpellCheckerPredictor(Predictor):
     def __init__(self,
                  model: Model,
                  dataset_reader: DatasetReader,
-                 use_ner_model: bool = False,
-                 ner_model: Optional[Callable] = None,
-                 named_entities: Set[str] = None):
+                 use_postprocessing: bool = True,
+                 distance_threshold: float = 0.2):
         super().__init__(model=model, dataset_reader=dataset_reader)
-        self.use_ner_model = use_ner_model
-        self.ner_model = ner_model
-        self.named_entities = named_entities or NAMED_ENTITIES
+        self.use_postprocessing = use_postprocessing
+        self.distance_threshold = distance_threshold
         self.labels = '.,'
         self.decoding_dict = {
             'COMMA': ',',
@@ -68,17 +82,64 @@ class SpellCheckerPredictor(Predictor):
 
     @overrides
     def _json_to_instance(self, json_dict: dict) -> Instance:
-        tokens = list(map(lambda x: x.rstrip('.,'), json_dict['tokens']))
+        tokens = list(map(lambda x: x.rstrip('.,').strip('"\'«»“”'), json_dict['tokens']))
         return self._dataset_reader.text_to_instance(tokens, add_noise=False, add_target=False)
 
     @staticmethod
     def __segment(boxes: List[str]) -> Iterator[List[str]]:
         return (sentence.split() for sentence in sent_tokenize(' '.join(boxes), 'russian'))
 
-    def _predict(self, json_dict: JsonDict) -> List[str]:
+    @staticmethod
+    def __check_punct(token: str) -> str:
+        if len(token) < 2:
+            return token
+
+        if token[0] in AFTER_PUNCT:
+            token = token[1:]
+
+        if token[-1] in BEFORE_PUNCT:
+            token = token[:-1]
+
+        return token
+
+    @staticmethod
+    def __check_single_char_word(token: str) -> str:
+        return SINGLE_CHAR_WORD.get(token, token)
+
+    @staticmethod
+    def __check_single_char_word_with_punct(token: str) -> str:
+        no_punct_token = ''.join([c for c in token if c not in punctuation])
+
+        if len(no_punct_token) == 1 and not no_punct_token.isdigit():
+            no_punct_token = SINGLE_CHAR_WORD.get(no_punct_token, no_punct_token)
+            token = ''.join([c if c in punctuation else no_punct_token for c in token])
+
+        return token
+
+    @staticmethod
+    def __check_double_char_word(token: str) -> str:
+        return DOUBLE_CHAR_WORD.get(token, token)
+
+    def __preprocess_input(self, json_dict: JsonDict) -> JsonDict:
+        tokens = []
+        for token in json_dict['tokens']:
+            token = token.strip()
+            token = self.__check_punct(token)
+            token = self.__check_single_char_word(token)
+            if len(token) > 1:
+                token = self.__check_single_char_word_with_punct(token)
+            token = self.__check_double_char_word(token)
+            tokens.append(token)
+
+        json_dict['tokens'] = tokens
+
+        return json_dict
+
+    def _predict(self, json_dict: JsonDict, heights: Sequence[float]) -> List[str]:
         if not json_dict:
             return []
 
+        json_dict = self.__preprocess_input(json_dict)
         predictions = self.predict_json(json_dict)
         predictions = [
             (self._model.vocab.get_token_from_index(
@@ -94,9 +155,8 @@ class SpellCheckerPredictor(Predictor):
                                                                predictions['mask']) if mask
         ]
 
-        entities = self.ner_model(json_dict['tokens']) if self.use_ner_model else None
-
-        return list(self.__decode_preds(json_dict['tokens'], predictions, entities))
+        return list(self.__decode_preds(json_dict['tokens'], predictions, heights)) \
+            if self.use_postprocessing else [x[0].replace('ё', 'е').replace('Ё', 'Е') for x in predictions]
 
     @staticmethod
     def __clean(token: str) -> str:
@@ -119,28 +179,40 @@ class SpellCheckerPredictor(Predictor):
         if casing_pattern == 'lower':
             return token.lower()
 
-        raise ValueError(f'Unknown casing pattern {casing_pattern}.')
+        raise ValueError(f'Unknown casing pattern: "{casing_pattern}".')
 
     def __replace_digits(self, token: str) -> str:
+        casing_pattern = [char.isupper() for char in token]
+        token_lower = token.lower()
+
         if self.all_digit2chars is None:
             digit2char = {value: [] for value in self.char2digit.values()}
             for key, value in self.char2digit.items():
                 digit2char[value].append(key)
 
-            self.all_digit2chars = [{key: value[idx] for idx, (key, value) in zip(indices, digit2char.items())}
+            self.all_digit2chars = [{k: v[idx] for idx, (k, v) in zip(indices, digit2char.items())}
                                     for indices in product(*[range(len(value)) for value in digit2char.values()])]
 
         for replacement_dict in self.all_digit2chars:
-            candidate = token[:]
+            candidate = token_lower[:]
             for key, value in replacement_dict.items():
                 candidate = candidate.replace(key, value)
-            if candidate.strip(punctuation).lower() in self.token_vocab_lowercase:
-                return candidate
+            if candidate in self.token_vocab_lowercase:
+                return self.__restore_casing(candidate, casing_pattern)
 
         return token
 
-    def __correct_token(self, token: str, candidate: str) -> str:
-        if candidate == '<DIGIT>':
+    def __is_close(self, token: str, candidate: str) -> bool:
+        return edit_distance(token.lower(), candidate.lower()) / len(candidate) <= self.distance_threshold
+
+    def __correct_token(self, token: str, candidate: str, height: float, mean_height: float) -> str:
+        if any(x in self.token_vocab_lowercase for x in (token.strip(punctuation).lower(),
+                                                         token.strip(punctuation),
+                                                         token.lower())):
+            return token
+
+        if candidate == '<DIGIT>' or token[0] in ('№', '§'):
+            token = token.lower()
             for key, value in self.char2digit.items():
                 token = token.replace(key, value)
             return token
@@ -149,7 +221,7 @@ class SpellCheckerPredictor(Predictor):
             casing_pattern = 'same'
         elif len(token) == len(candidate):
             casing_pattern = [char.isupper() for char in token]
-        elif token.isupper():
+        elif token.isupper() and height > 1.2 * mean_height:
             casing_pattern = 'upper'
         elif token.istitle():
             casing_pattern = 'capitalized'
@@ -159,7 +231,7 @@ class SpellCheckerPredictor(Predictor):
         token = self.__replace_digits(token)
 
         if len(token) < self._dataset_reader.min_noise_length \
-                or edit_distance(token.lower(), candidate.lower()) > min(len(token), len(candidate)) * 0.4 \
+                or not self.__is_close(token, candidate) \
                 or candidate in ('<LATIN>', self.oov_token) \
                 or any(char not in self.character_vocab for char in token.lower()):
             return token
@@ -168,11 +240,12 @@ class SpellCheckerPredictor(Predictor):
 
         return candidate
 
-    def __decode_preds(self, tokens: List[str], preds: List[Tuple[str, str]],
-                       entities: Union[List[str], None]) -> Iterator[str]:
-        for i, (token, (candidate, punct_label)) in enumerate(zip(tokens, preds)):
+    def __decode_preds(self, tokens: List[str], preds: List[Tuple[str, str]], heights: Sequence[float]) -> Iterator[str]:
+        mean_height = np.mean(heights).item()
 
-            if all(char in punctuation for char in token):
+        for i, (token, (candidate, _)) in enumerate(zip(tokens, preds)):
+
+            if not token or all(char in punctuation for char in token):
                 yield token
                 continue
 
@@ -180,24 +253,27 @@ class SpellCheckerPredictor(Predictor):
             right_punctuation = punctuation_to_keep.match(token[::-1]).groups()[0][::-1]
 
             clean_token = self.__clean(token)
-            corrected_token = self.__correct_token(clean_token, candidate) \
-                if entities is None or entities[i][2:] not in self.named_entities else clean_token
+            corrected_token = self.__correct_token(clean_token, candidate, heights[i], mean_height)
             corrected_token = left_punctuation + corrected_token + right_punctuation
 
-            if token.endswith(','):
-                corrected_token += ','
-            elif token.endswith(self.decoding_dict[punct_label]):
-                corrected_token += self.decoding_dict[punct_label]
+            if token.endswith((',', '.')):
+                corrected_token += token[-1]
 
-            yield corrected_token
+            yield corrected_token.replace('ё', 'е').replace('Ё', 'Е')
 
-    def predict(self, boxes: List[str]) -> List[str]:
-        if not boxes:
-            return []
+    def predict(self, boxes: List[str], heights: Sequence[float], segment: bool = False) -> List[str]:
+        if all(not token for token in boxes):
+            return boxes
 
-        predictions = []
-        for sent_tokens in self.__segment(boxes):
-            sent_preds = self._predict({'tokens': sent_tokens})
-            predictions.extend(sent_preds)
+        if segment:
+            i = 0
+            predictions = []
+            for sent_tokens in self.__segment(boxes):
+                sent_heights = heights[i:i + len(sent_tokens)]
+                sent_preds = self._predict({'tokens': sent_tokens}, sent_heights)
+                predictions.extend(sent_preds)
+                i += len(sent_tokens)
+        else:
+            predictions = self._predict({'tokens': boxes}, heights)
 
         return predictions
